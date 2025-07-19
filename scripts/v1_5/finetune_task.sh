e789408985087ece351182a80e283c5710e762d4
Vipul Pandey <54221581+vipulpandey12345@users.noreply.github.com>
2025-07-19 00:34:35 -0700

    changes

--- scripts/v1_5/finetune_task.sh
+++ scripts/v1_5/finetune_task.sh
@@ -36,4 +36,5 @@ deepspeed llava/train/train_mem.py \
     --gradient_checkpointing True \
     --dataloader_num_workers 0 \
     --lazy_preprocess True \
-    --report_to wandb
+    --report_to wandb \
+    --deepspeed scripts/zero3_offload.json
