72ce8c722bdc44ca35a7947e8849fc74b2354657
Vipul Pandey <54221581+vipulpandey12345@users.noreply.github.com>
2025-07-19 00:52:22 -0700

    changes

--- scripts/v1_5/finetune_without_deepspeed.sh
+++ scripts/v1_5/finetune_without_deepspeed.sh
@@ -20,7 +20,6 @@ python llava/train/train_mem.py \
     --per_device_train_batch_size 1 \
     --per_device_eval_batch_size 4 \
     --gradient_accumulation_steps 4 \
-    --evaluation_strategy "no" \
     --save_strategy "steps" \
     --save_steps 50000 \
     --save_total_limit 1 \
